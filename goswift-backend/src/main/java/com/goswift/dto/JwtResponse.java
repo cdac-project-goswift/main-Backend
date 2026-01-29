0d5e54db81a9d5254da81501af3e5e3ad1393b83
Ishwari Dubewar <ishwaridubewar2793@gmail.com>
2026-01-29 23:51:46 +0530

    added jwt,user,req DTOs

--- goswift-backend/src/main/java/com/goswift/dto/JwtResponse.java
+++ goswift-backend/src/main/java/com/goswift/dto/JwtResponse.java
@@ -3,6 +3,12 @@ package com.goswift.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/*
+user to send the token 
+to the client side after successfuly login
+token
+user
+*/
 @Data
 @AllArgsConstructor
 public class JwtResponse {
