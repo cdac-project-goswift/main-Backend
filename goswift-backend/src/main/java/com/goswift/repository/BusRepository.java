46b24ce96ecd92a5edaa042a7501c67d4c7e1ad2
Ishwari Dubewar <ishwaridubewar2793@gmail.com>
2026-01-30 22:36:51 +0530

    added some agent funtionality - addBus ,getMyBuses , updateBus

--- goswift-backend/src/main/java/com/goswift/repository/BusRepository.java
+++ goswift-backend/src/main/java/com/goswift/repository/BusRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BusRepository extends JpaRepository<Bus, Long> {
-    
+    List<Bus> findByAgency(Agency agency);
     
 }
