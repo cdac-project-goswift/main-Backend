8fd7f5d2cdfbb784b2b6fddae4b951630db00abc
gururajtakale <gururajtakale0007@gmail.com>
2026-02-06 23:39:40 +0530

    Updated AppConfig And WebConfig Files

--- goswift-backend/src/main/java/com/goswift/config/WebConfig.java
+++ goswift-backend/src/main/java/com/goswift/config/WebConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -14,5 +16,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+              
     }
 }
