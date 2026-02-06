883e9fba8c8df7943005546bbbde26c33fba1779
Bholerohan6055 <bhole.rohan6055@gmail.com>
2026-02-06 21:38:52 +0530

    Add custom query to SystemConfigRepository
    Added a custom query to retrieve the global system configuration.

--- goswift-backend/src/main/java/com/goswift/repository/SystemConfigRepository.java
+++ goswift-backend/src/main/java/com/goswift/repository/SystemConfigRepository.java
@@ -3,7 +3,12 @@ package com.goswift.repository;
 import com.goswift.entity.SystemConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SystemConfigRepository extends JpaRepository<SystemConfig, Integer> {
     
+    // This custom query was missing and causing the build error
+    @Query(value = "SELECT * FROM system_config LIMIT 1", nativeQuery = true)
+    SystemConfig getGlobalConfig();
 }
